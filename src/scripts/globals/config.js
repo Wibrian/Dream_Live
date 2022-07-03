eab6c9f12c028c662ea90a8869014fe91025274c
KemalVX <76717483+KemalYF@users.noreply.github.com>
2022-07-03 13:36:39 +0700

    Update key
    update key config

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
   BASE_URL: 'https://hotels4.p.rapidapi.com/',
-  KEY: '804f1a99a9msh7feca5a1c518010p19c5f3jsnc2f38df2a342',
+  KEY: 'c4cb3e5b73msh0e77399a732d200p151702jsn87304d14fff1',
   DEFAULT_LANGUAGE: 'en-us',
   CACHE_NAME: 'Dream-Live-Cache',
   DATABASE_NAME: 'dream-live-database',
